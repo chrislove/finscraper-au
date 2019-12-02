7dbca6edd1a17bcdad4b1f56e904f19740ff566a
Chris Love <chris@love.name>
2019-12-02 13:18:24 +1000

    Scraper for St George

---
+++ FinScraper/ScrapeCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinScraper
+{
+    public class ScrapeResponse
+    {
+        public string Result { get; }
+
+        public ScrapeResponse(string result)
+        {
+            Result = result;
+        }
+    }
+}
---
+++ FinScraper/ScrapeOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinScraper
+{
+    public class ScrapeOptions
+    {
+        public string FromDate;
+        public string ToDate;
+        public LoginCredentials LoginCredentials;
+
+        public ScrapeOptions(LoginCredentials loginCredentials, string fromDate, string toDate)
+        {
+            FromDate = fromDate;
+            ToDate = toDate;
+            LoginCredentials = loginCredentials;
+        }
+
+    }
+}
